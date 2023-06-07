e91a18800ae384b6a1e7ea591f1fac03cb7053bd
natxofp <ignasi.fernandez@chalmers.se>
2023-06-07 09:59:39 +0200

    h

--- Azure_CosmosDB/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-settings = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://sensit.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY', 'vSnfmNb3YYAzCTxwVMK2lQaroICMWW3hNauqjc6ROjEQHHduBToI4RlWXZLIplSxxeyGR0XBugm0ACDbmkaJxQ=='),
-    'database_id': os.environ.get('COSMOS_DATABASE', 'KT_2023'),
-    'container_id': os.environ.get('COSMOS_CONTAINER', 'Balk_1'),
-}
